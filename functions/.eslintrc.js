a2ac9c2605e034b46f626d91631506ce4e176240
Eduard Alexandru <eduard.github@gmail.com>
2022-08-03 22:53:47 +0100

    updated .eslinkrc.js, firebase.json, index.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "google",
-  ],
-  rules: {
-    quotes: ["error", "double"],
-  },
+    root: true,
+    env: {
+        es6: true,
+        node: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "google",
+    ],
+    parserOptions: {
+        sourceType: "module",
+        parser: "babel-eslint",
+        ecmaVersion: 8,
+    },
+    rules: {
+        'quotes': ["off", "double"],
+        'indent': ["error", 4],
+        'max-len': ["warn", {"code": 80}],
+    },
 };
--- functions/index.js
+++ functions/index.js
@@ -10,17 +10,16 @@
 // });
 
 
-const admin = require('firebase-admin');
-const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
 admin.initializeApp();
 const db = admin.firestore();
 
 
-export const app = functions.https.onRequest(async (req, res) => {
-    
+exports.app = functions.https.onRequest(async (req, res) => {
     // save every request. use it for analytics purposes.
     await db
-        .collection('requests')
+        .collection("requests")
         .add({
             headers: req.headers,
             timestamp: admin.firestore.FieldValue.serverTimestamp(),
@@ -30,31 +29,31 @@ export const app = functions.https.onRequest(async (req, res) => {
 
     // keep count of how many requests have been made
     await db
-        .doc('/counters/requests')
+        .doc("/counters/requests")
         .set({
-            count: admin.firestore.FieldValue.increment(1)
+            count: admin.firestore.FieldValue.increment(1),
         }, {
-            merge: true
+            merge: true,
         })
         .catch((e) => console.error(e));
 
     // this is optional, but should be the right way.
     // redirect to some landing page.
-    if (req.path === '/') return res.redirect('https://gtto.site/info.html');
+    if (req.path === "/") return res.redirect("https://gtto.site/info.html");
 
     // pseudo-landing page, just return some information page
-    if (req.path === '/info.html') return res.send("Hello! This is a test to prove accessing /info.html works.");
+    if (req.path === "/info.html") return res.send("Hello! This is a test to prove accessing /info.html works.");
 
     // check firestore database if there is any valid short link
     const snap = await db
-        .collection('links')
-        .where('short', '==', req.path.split('/')[1])
+        .collection("links")
+        .where("short", "==", req.path.split("/")[1])
         .get()
         .catch((e) => console.error(e));
 
     // redirect as appropriate
     if (!snap || snap.size === 0) {
-        res.status(404).send('Invalid short link');
+        res.status(404).send("Invalid short link");
     } else {
         res.redirect(snap.docs[0].data().long);
     }
@@ -62,30 +61,30 @@ export const app = functions.https.onRequest(async (req, res) => {
 
 
 // keep count of newly added links
-export const increaseLinkCount = functions.firestore
-    .document('/links/{linkId}')
+exports.increaseLinkCount = functions.firestore
+    .document("/links/{linkId}")
     .onCreate(async () => {
         await db
-            .doc('/counters/links')
+            .doc("/counters/links")
             .set({
-                count: admin.firestore.FieldValue.increment(1)
+                count: admin.firestore.FieldValue.increment(1),
             }, {
-                merge: true
+                merge: true,
             })
             .catch((e) => console.error(e));
     });
 
 
 // keep count of deleted links
-export const reduceLinkCount = functions.firestore
-    .document('/links/{linkId}')
+exports.reduceLinkCount = functions.firestore
+    .document("/links/{linkId}")
     .onDelete(async () => {
         await db
-            .doc('/counters/links')
+            .doc("/counters/links")
             .set({
-                count: admin.firestore.FieldValue.increment(-1)
+                count: admin.firestore.FieldValue.increment(-1),
             }, {
-                merge: true
+                merge: true,
             })
             .catch((e) => console.error(e));
     });
